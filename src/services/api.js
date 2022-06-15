6dcc6cea95448305bbfbe9fea1367d86fdce4cde
Emanuel Ngola <emanuelngola33@gmail.com>
2022-06-15 09:46:34 +0100

    change api URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 
 const api = axios.create({
-  //baseURL: "https://helptask-api.herokuapp.com/api/",
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://helptask-api.herokuapp.com/api/",
+  //baseURL: "http://localhost:8000/api/",
 });
 
 export default api;
